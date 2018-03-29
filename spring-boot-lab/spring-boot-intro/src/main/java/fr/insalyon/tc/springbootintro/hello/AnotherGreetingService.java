753da0ff355166c9458a03b55e0bcb7d9559ca56
Brian Clozel <bclozel@pivotal.io>
2018-03-29 08:28:05 +0200

    DI Edge case: multiple candidates
    We now have multiple candidates for a single injection point. Since we
    haven't given any clue about our preference here, Spring fails the
    application at startup with the following:
    
    ```
    Parameter 0 of constructor in fr.insalyon.tc.springbootintro.GreetingRunner
    required a single bean, but 2 were found:
    ...
    Action:
    
    Consider marking one of the beans as @Primary, updating the consumer to
    accept multiple beans, or using @Qualifier to identify the bean that
    should be consumed
    ```

--- spring-boot-lab/spring-boot-intro/src/main/java/fr/insalyon/tc/springbootintro/hello/AnotherGreetingService.java
+++ spring-boot-lab/spring-boot-intro/src/main/java/fr/insalyon/tc/springbootintro/hello/AnotherGreetingService.java
@@ -3,10 +3,10 @@ package fr.insalyon.tc.springbootintro.hello;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SimpleGreetingService implements GreetingService {
+public class AnotherGreetingService implements GreetingService {
 
 	@Override
 	public String greet(String name) {
-		return String.format("Hello, %s!", name);
+		return String.format("Bonjour, %s!", name);
 	}
 }
