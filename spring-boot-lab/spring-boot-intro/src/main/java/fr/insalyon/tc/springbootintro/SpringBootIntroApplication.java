7becfca6a93906a8f1cc9d47030b14609ce453c8
Brian Clozel <bclozel@pivotal.io>
2018-03-29 08:15:04 +0200

    Create Spring Boot Intro application
    Using https://start.spring.io, we create a new Spring Boot application.

---
+++ spring-boot-lab/spring-boot-intro/src/main/java/fr/insalyon/tc/springbootintro/SpringBootIntroApplication.java
@@ -0,0 +1,12 @@
+package fr.insalyon.tc.springbootintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntroApplication.class, args);
+	}
+}
