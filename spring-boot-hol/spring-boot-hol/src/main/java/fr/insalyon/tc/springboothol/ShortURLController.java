a8f028c49ca473d3447ca1cc114ac550b8394e5c
Brian Clozel <bclozel@pivotal.io>
2018-03-30 17:13:59 +0200

    Create our first Controller
    Here, we're just copying/pasting the code snippet from the lab script.

---
+++ spring-boot-hol/spring-boot-hol/src/main/java/fr/insalyon/tc/springboothol/ShortURLController.java
@@ -0,0 +1,38 @@
+package fr.insalyon.tc.springboothol;
+
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
+
+@Controller
+public class ShortURLController {
+
+	private final ShortURLService shortURLService;
+
+	@Autowired
+	public ShortURLController(ShortURLService shortURLService) {
+		this.shortURLService = shortURLService;
+	}
+
+	@PostMapping("/")
+	public ResponseEntity shortenURL(@RequestParam URI uri) {
+		ShortURL shortURL = this.shortURLService.shortenURL(uri);
+		URI shortenedURL = URI.create("http://localhost:8080/" + shortURL.getShortCode());
+		// the host information is hard coded here for simplicity
+		// but you can do better with MvcUriComponentsBuilder::fromMethodCall
+		return ResponseEntity.created(shortenedURL).build();
+	}
+
+}
