50eef14b901041789e1a4021745eea0f1195cbd9
Brian Clozel <bclozel@pivotal.io>
2018-03-30 14:17:25 +0200

    Add Spring Boot HOL application

---
+++ spring-boot-hol/spring-boot-hol/src/main/java/fr/insalyon/tc/springboothol/SpringBootHolApplication.java
@@ -0,0 +1,12 @@
+package fr.insalyon.tc.springboothol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHolApplication.class, args);
+	}
+}
---
+++ spring-boot-hol/spring-boot-hol/src/test/java/fr/insalyon/tc/springboothol/SpringBootHolApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.insalyon.tc.springboothol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootHolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
